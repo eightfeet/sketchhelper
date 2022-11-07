4ff858294611c56bf567110878aae33fa80df8d3
eightfeet <bajiao000000@163.com>
2022-11-07 14:47:28 +0800

    新增组件extend参数

--- src/components/GuideGroup/CircleCross/CircleCross.tsx
+++ src/components/GuideGroup/CircleCross/CircleCross.tsx
@@ -9,49 +9,131 @@ interface Props extends GroupProps {
     cross?: boolean;
     color?: string;
     outerRing?: boolean;
+    extend?: number;
+    extendCone?: boolean;
+    lineWidth?: number;
 }
 
-const CircleCross:React.FC<Props> = ({radius=1, segments=8, cross, color="#000", outerRing, ...other}) => {
-    const points = new THREE.Path().absarc(0, 0, radius, 0, Math.PI * 2, true).getSpacedPoints(segments);
+const CircleCross: React.FC<Props> = ({
+    extend = 0,
+    radius = 1,
+    segments = 8,
+    cross,
+    color = '#000',
+    outerRing,
+    extendCone,
+    lineWidth=1.5,
+    ...other
+}) => {
+    const points = new THREE.Path()
+        .absarc(0, 0, radius, 0, Math.PI * 2, true)
+        .getSpacedPoints(segments);
     const renderCross = useCallback(
-      () => {
-        if (points.length % 2 === 0 || cross !== true) return null;
-        const crossPoints = points.slice(1);
-        const linesNode: React.ReactNode[]  = [];
-        const half = crossPoints.length/2;
-        for (let index = 0; index < half; index++) {
-            const pointA = crossPoints[index];
-            const pointB = crossPoints[index + half];
-            linesNode.push(<Line color={color} key={index} points={[[pointA.x, pointA.y, 0], [pointB.x, pointB.y, 0]]}>asd</Line>);
-        }
-        return linesNode;
-      },
-      [points, cross, color],
-    )
+        () => {
+            if (points.length % 2 === 0 || cross !== true) return null;
+            const crossPoints = points.slice(1);
+            const linesNode: React.ReactNode[] = [];
+            const half = crossPoints.length / 2;
+            for (let index = 0; index < half; index++) {
+                const pointA = crossPoints[index];
+                const pointB = crossPoints[index + half];
+                linesNode.push(
+                    <Line
+                        color={color}
+                        key={index}
+                        lineWidth={lineWidth}
+                        points={[
+                            [pointA.x, pointA.y, 0],
+                            [pointB.x, pointB.y, 0],
+                        ]}
+                    />
+                );
+            }
+            return linesNode;
+        },
+        [points, cross, color, lineWidth]
+    );
 
     const renderOuterRing = useCallback(
-      () => outerRing ? <Line
+        () =>
+            outerRing ? (
+                <Line
+                    rotation={[0, 0, 0]}
+                    points={new THREE.Path()
+                        .absarc(0, 0, radius, 0, Math.PI * 2, true)
+                        .getSpacedPoints(60)}
+                    color={color}
+                    lineWidth={lineWidth}
+                />
+            ) : null,
+        [color, outerRing, radius, lineWidth]
+    );
+
+    const renderExtends = useCallback(() => {
+        if (!extend) return null;
+        const crossPoints = points.slice(1);
+        const extendNode: React.ReactNode[] = [];
+        crossPoints.forEach((element, index) => {
+            extendNode.push(
+                <Line
+                    color={color}
+                    key={index}
+                    lineWidth={lineWidth}
+                    points={[
+                        [element.x, element.y, 0],
+                        [element.x, element.y, extend],
+                    ]}
+                />
+            );
+            if (extendCone) {
+                extendNode.push(
+                    <Line
+                        color={color}
+                        lineWidth={lineWidth}
+                        key={`cone${index}`}
+                        points={[
+                            [element.x, element.y, 0],
+                            [0, 0, extend],
+                        ]}
+                    />
+                );
+            }
+        });
+        extendNode.push(<Line
+            key={crossPoints.length + 1}
             rotation={[0, 0, 0]}
-            points={new THREE.Path().absarc(0, 0, radius, 0, Math.PI * 2, true).getSpacedPoints(60)}
+            points={[[0,0,0],[0,0,extend]]}
             color={color}
-            lineWidth={1.5}
-        /> : null,
-      [color, outerRing, radius],
-    )
-    
-    
+            lineWidth={lineWidth}
+        />)
+        extendNode.push(
+            <group key="extendcap" position={[0,0,extend]}>
+                {renderCross()}
+                {renderOuterRing()}
+                <Line
+                    rotation={[0, 0, 0]}
+                    points={points}
+                    color={color}
+                    lineWidth={lineWidth}
+                />
+            </group>
+        );
+        return extendNode;
+    }, [color, extend, extendCone, points, renderCross, renderOuterRing]);
+
     return (
         <group {...other}>
             <Line
                 rotation={[0, 0, 0]}
                 points={points}
                 color={color}
-                lineWidth={1.5}
+                lineWidth={lineWidth}
             />
             {renderOuterRing()}
             {renderCross()}
+            {renderExtends()}
         </group>
-    )
-}
+    );
+};
 
 export default CircleCross;
