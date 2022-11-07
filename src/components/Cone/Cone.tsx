c0210fe6d7fc7be4e875e8bdd98a2cc2d9aa3251
eightfeet <bajiao000000@163.com>
2022-11-07 23:14:59 +0800

    调整结构线

--- src/components/Cone/Cone.tsx
+++ src/components/Cone/Cone.tsx
@@ -1,6 +1,7 @@
 import { Line } from '@react-three/drei';
 import React from 'react';
 import GuideGroup from '../GuideGroup';
+import CircleCross from '../GuideGroup/CircleCross';
 
 interface Props {
     position?: number[];
@@ -14,123 +15,61 @@ const Cone: React.FC<Props> = ({ position, opacity, showEdige, shadow }) => {
         <mesh
             castShadow={shadow}
             // receiveShadow={shadow}
-            position={[0, 0.5, 0]}
+            position={[0, 0.26, 0]}
         >
-            <coneGeometry args={[1, 3, 100]} />
+            <coneGeometry args={[1, 2.5, 100]} />
             <meshStandardMaterial
                 transparent
                 opacity={opacity}
                 metalness={0.1}
             />
             {showEdige && <>
-                <GuideGroup color='red' position={[0, -1.49, 0]} rotation={[-1.57, 0, 0]}></GuideGroup>
-                <GuideGroup color='red' position={[0, 1.49, 0]} rotation={[-1.57, 0, 0]}></GuideGroup>
 
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [1, 1.5, -1],
-                        [1, -1.5, -1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, -0],
-                        [1, -1.5, 0],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [1, 1.5, -0],
-                        [1, -1.5, 0],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, -0],
-                        [0, -1.5, -1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, 0],
-                        [0, -1.5, 0],
-                    ]}
-                    color="red"
-                />
-
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, -1],
-                        [0, -1.5, -1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [-1, 1.5, -1],
-                        [-1, -1.5, -1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [1, 1.5, 1],
-                        [1, -1.5, 1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [-1, 1.5, 1],
-                        [-1, -1.5, 1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, 0],
-                        [0, -1.5, 1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, 1],
-                        [0, -1.5, 1],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [0, 1.5, 0],
-                        [-1, -1.5, 0],
-                    ]}
-                    color="red"
-                />
-                <Line
-                    rotation={[0, 0, 0]}
-                    points={[
-                        [-1, 1.5, 0],
-                        [-1, -1.5, 0],
-                    ]}
-                    color="red"
-                />
+            
+            <CircleCross
+                segments={4}
+                cross
+                position={[0, -0.245, 0]}
+                color={'red'}
+                extend={2.5}
+                extendRadius={0}
+                radius={1}
+                innerRing={false}
+                outerRing
+                lineWidth={2.5}
+            />
+            <CircleCross
+                segments={4}
+                cross
+                position={[0, -0.245, 0]}
+                color={'red'}
+                extend={1.2}
+                extendRadius={0.52}
+                radius={1}
+                innerRing={false}
+                outerRing
+                lineWidth={2.5}
+            />
+            <CircleCross
+                segments={4}
+                color={'#666'}
+                cross
+                position={[0, -0.245, 0]}
+                rotation={[0,0.79,0]}
+                extend={2.5}
+                extendCone
+                radius={1.4}
+                lineWidth={2}
+            />
+            <CircleCross
+                segments={4}
+                color={'#666'}
+                cross
+                position={[0, 0.955, 0]}
+                rotation={[0,0.79,0]}
+                radius={0.735}
+                lineWidth={2}
+            />
             </>}
         </mesh>
     );
--- src/components/Shape/index.tsx
+++ src/components/Shape/index.tsx
@@ -1,5 +1,6 @@
 import { Center, Edges, useGLTF } from '@react-three/drei';
 import React from 'react';
+import CircleCross from '../GuideGroup/CircleCross';
 
 interface Props {
     position?: number[];
@@ -14,13 +15,13 @@ interface Props {
 
 const Shape: React.FC<Props> = ({ position, opacity, showEdige, name }) => {
     const { nodes } = useGLTF(`${process.env.PUBLIC_URL || ''}/glb/shape.glb`) as any;
-    
+
 
     const groups = {
-        shapea: [nodes.shapea.geometry, [0, 0, 0],0.5],
-        shapeb: [nodes.shapeb.geometry, [1.23, 0.72, 0.5],0.5],
-        shapec: [nodes.shapec.geometry, [1.58, 0, 0.8],0.5],
-        shaped: [nodes.shaped.geometry, [0, 0, 0],0.5],
+        shapea: [nodes.shapea.geometry, [0, 0, 0], 0.5],
+        shapeb: [nodes.shapeb.geometry, [1.23, 0.72, 0.5], 0.5],
+        shapec: [nodes.shapec.geometry, [1.58, 0, 0.8], 0.5],
+        shaped: [nodes.shaped.geometry, [0, 0, 0], 0.5],
     }
 
     const current = groups[name];
@@ -38,11 +39,16 @@ const Shape: React.FC<Props> = ({ position, opacity, showEdige, name }) => {
                 <meshStandardMaterial transparent opacity={1} roughness={0.7} metalness={0.25} envMapIntensity={0.5} />
 
                 {!!showEdige && (
-                    <Edges
-                        threshold={5} // Display edges only when the angle between two faces exceeds this value (default=15 degrees)
-                        color="red"
-                    />
+                    <>
+                    {name==='shapea' && 
+                        <>
+                            <CircleCross radius={2.35} rotation={[0, 0.526, 0]} segments={6} position={[0, -1.18, 0]} color="red" outerRing extendCone cross extend={4.35} lineWidth={2} />
+                            <CircleCross radius={3.35} rotation={[0, 0.79, 0]} segments={4} position={[0, -1.18, 0]} color="#333" outerRing={false} innerRing lineWidth={2} extend={4.35} />
+                        </>
+                    }
+                    </>
                 )}
+
             </mesh>
         </Center>
     );
