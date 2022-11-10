8685986df4a4fb51355af8de8b55f2cf5ca17e97
eightfeet <bajiao000000@163.com>
2022-11-11 07:17:16 +0800

    update

--- src/components/obj/ConeCross/ConeCross.tsx
+++ src/components/obj/ConeCross/ConeCross.tsx
@@ -14,13 +14,13 @@ const ConeCross: React.FC<Props> = ({ position, opacity, showEdige, shadow }) =>
     const { nodes } = useGLTF(`${process.env.PUBLIC_URL || ''}/glb/conecross.glb`) as any;
 
     console.log(nodes);
-    
+
 
 
     return (
         <>
             <mesh
-            castShadow={shadow}
+                castShadow={shadow}
                 receiveShadow={shadow}
                 position={[0, 0.26, 0]}
             >
@@ -32,10 +32,10 @@ const ConeCross: React.FC<Props> = ({ position, opacity, showEdige, shadow }) =>
                 />
             </mesh>
             <mesh
-            castShadow={shadow}
+                castShadow={shadow}
                 receiveShadow={shadow}
                 position={[0, 0.26, 0]}
-                rotation={[1.57,0,0]}
+                rotation={[1.57, 0, 0]}
             >
                 <cylinderGeometry args={[0.46, 0.46, 2, 100]} />
                 {/* <boxGeometry args={[2, 0.7, 0.7]} /> */}
@@ -45,14 +45,13 @@ const ConeCross: React.FC<Props> = ({ position, opacity, showEdige, shadow }) =>
                     metalness={0.1}
                 />
             </mesh>
-            
-            {!!showEdige && (
-                <>
-                    <CircleCross lineWidth={2.0} cross extendRadius={0} radius={1.01} extendCone innerRing={false} outerRing position={[0,-1,0]} extend={2.52} segments={4} />
-                    <SquareCross lineWidth={2.0} width={2.01} height={2.01} position={[0,-1,0]} extend={2.52} color="#777" />
-                    <SquareCross lineWidth={2.0} width={0.92} height={0.92} rotation={[1.57,0,0]} position={[0,0.26,-1]} extend={2.0} color="#777" />
-                    <CircleCross lineWidth={2.0} radius={0.465} cross rotation={[1.57,0,0]} innerRing={false} outerRing position={[0,0.26,-1]} extend={2.0} segments={4}  />
-                </>
+
+            {!!showEdige && (<>
+                    <CircleCross lineWidth={2.0} cross extendRadius={0} radius={1.01} extendCone innerRing={false} outerRing position={[0, -1.005, 0]} extend={2.52} color="red" segments={4} />
+                    <SquareCross lineWidth={2.0} width={2.01} height={2.01} position={[0, -1, 0]} extend={2.52} color="#777" />
+                    <SquareCross lineWidth={2.0} width={0.92} height={0.92} rotation={[1.57, 0, 0]} position={[0, 0.26, -1]} extend={2.0} color="#777" />
+                    <CircleCross color='red' lineWidth={2.0} radius={0.465} cross rotation={[1.57, 0, 0]} innerRing={false} outerRing position={[0, 0.26, -1.005]} extend={2.01} segments={4} />
+            </>
             )}
         </>
     );
