f3884eb61440bc564251d19bb639caa787d021d3
eightfeet <bajiao000000@163.com>
2022-12-07 16:45:22 +0800

    update

--- src/components/obj/body/SkeletonHead/SkeletonHead.tsx
+++ src/components/obj/body/SkeletonHead/SkeletonHead.tsx
@@ -116,14 +116,6 @@ const SkeletonHead: React.FC<Props> = ({ visible, opacity, showEdige, shadow, ..
                     </mesh>
 
                 </group >
-                {!!showEdige && (
-                    <group rotation={[0, angleToRotation(-5), 0]}>
-                        <StandardCross lineWidth={2.5} color='#000' radius={1} position={[0, 8.13, 0]} isSquare={false} />
-                        <StandardCross lineWidth={2.5} color='#000' radius={1} position={[0, 8.13, 0]} rotation={[angleToRotation(90), 0, 0]} isSquare={false} />
-                        <StandardCross lineWidth={2.5} color='#000' radius={1} position={[0, 8.13, 0]} rotation={[0, 0, angleToRotation(90)]} isSquare={false} />
-
-                    </group>
-                )}
             </Center>
         </group>
     );
