212c194018c0bd426dc924a775b327384d939225
eightfeet <bajiao000000@163.com>
2022-12-08 10:46:06 +0800

    整理已有模型

--- src/components/StilLife/objList.ts
+++ src/components/StilLife/objList.ts
@@ -35,6 +35,6 @@ export const objList = [
     { component: 'body/Skeleton', name: 'skeleton', label: '骷髅', thumbnail: './glb/thumbnail/body/skeleton.png' },
     { component: 'body/SkeletonB', name: 'skeleton_b', label: '骷髅', thumbnail: './glb/thumbnail/body/skeleton_b.png' },
     { component: 'LoomisHead', name: 'loomis_head', label: 'loomis_head', thumbnail: './glb/thumbnail/body/loomis_head.png' },
-    { component: 'body/SkeletonHead', name: 'skeleton_head', label: 'skeleton_head', thumbnail: './glb/body/thumbnail/skeleton_head.png' },
+    { component: 'body/SkeletonHead', name: 'skeleton_head', label: 'skeleton_head', thumbnail: './glb/thumbnail/body/skeleton_head.png' },
 ];
 
